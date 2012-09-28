089439dbb28936e167fcc0397bd2d8f0d4441b38
omer <omer@omer.com>
2012-09-28 13:49:41 +0200

    Add funcs

--- helpers/bus_calendar.py
+++ helpers/bus_calendar.py
@@ -1,6 +1,9 @@
 import db
 
-def find(day):
+days = {'sunday': None, 'monday': None, 'tuesday': None, 'wednesday': None, 'thursday': None, 'friday': None, 'saturday': None}
+
+
+def find_all(day):
     sql = 'select service_id from calendar where {0} = 1'.format(day)
     return db.get_trips_ids('service_id',db.First(db.Query(sql)))
 
@@ -8,3 +11,14 @@ def find(day,trip_ids):
     s = "'" + "','".join(trip_ids) + "'"
     sql = 'select trips.trip_id from trips join calendar as c on trips.service_id = c.service_id where c.{0} = 1 and trips.trip_id in ({1})'.format(day,s)
     return db.First(db.Query(sql))
+
+def get_all_per_day(days):
+    sql = 'select service_id from calendar where {0} = 1'
+    for day in days:
+        days[day] = db.First(db.Query(sql.format(day)))
+    return days
+
+days = get_all_per_day(days)
+
+def get_ids(day):
+    return days[day]
