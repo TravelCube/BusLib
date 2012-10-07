5a2e53fcd80431e2cf6ce8d72d7a8eb0dee3e076
omer <omer@omer.com>
2012-10-07 13:16:09 +0200

    changes for mvc like

--- helpers/trips.py
+++
@@ -1,7 +0,0 @@
-import db
-
-def get_routes(trips_ids):
-    s = "'" + "','".join(trips_ids) + "'"
-    sql = 'select route_id,trip_id from trips where trip_id in ({0})'.format(s)
-    l = db.Query(sql)
-    return l
