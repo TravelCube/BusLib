b553162c1ec961aab61c5d34d9b281a99bdffd53
omer <omer@omer.com>
2012-10-19 19:15:02 +0200

    new db schema

--- stops.py
+++ stops.py
@@ -11,7 +11,7 @@ def get_stops_lat_lon(ids):
 
 def get_stop_files_last_station(file_names):
     if len(file_names) == 1:
-        sql = "select file_name,last from stops_files_stations where file_name = '{0}'".format(file_names[0])
+        sql = "select file_name,last_station from files_last_station where file_name = '{0}'".format(file_names[0])
     else:
-        sql = 'select file_name,last from stops_files_stations where file_name in {0}'.format(tuple(file_names))
+        sql = 'select file_name,last_station from files_last_station where file_name in {0}'.format(tuple(file_names))
     return db.Query(sql)
